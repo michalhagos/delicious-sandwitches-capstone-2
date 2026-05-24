74fcfcc2e75ca433e92020e578bb446aded7dc32
michalhagos <mhayele3@gmail.com>
2026-05-23 21:58:10 -0500

    Add constructor to Sandwich that sets base price from size

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -15,7 +15,24 @@ public class Sandwich {
         // the starting price of the sandwich based on size only.topping prices get added on top of this
         private double basePrice;
 
+    // constructor sets bread size and toasted. base price is  calculated from the size
+    public Sandwich(String bread, int size, boolean toasted) {
+        this.bread = bread;
+        this.size = size;
+        this.toasted = toasted;
 
+        // initializing the toppings list so it is ready to hold toppings
+        this.toppings = new ArrayList<>();
+
+        // set the base price based on the size chosen by the customer
+        if (size == 4) {
+            this.basePrice = 5.50;
+        } else if (size == 8) {
+            this.basePrice = 7.00;
+        } else if (size == 12) {
+            this.basePrice = 8.50;
+        }
+    }
 
 
 
