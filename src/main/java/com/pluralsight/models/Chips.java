01fe429d29336427da86f73c22e281afb65a79dc
michalhagos <mhayele3@gmail.com>
2026-05-24 15:17:36 -0500

    Added private fields type and price to Chips class

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -1,4 +1,8 @@
 package com.pluralsight.models;
 
 public class Chips {
+    // the type of chips chosen by customer
+    private String type;
+    // chips are always a flat price of $1.50
+    private double price = 1.50;
 }
