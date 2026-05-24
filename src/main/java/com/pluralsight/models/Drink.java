006c2452d1629c4e82262dcc886c54519f810d24
michalhagos <mhayele3@gmail.com>
2026-05-24 00:13:25 -0500

    Add private fields size flavor and price to Drink class

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,4 +1,11 @@
 package com.pluralsight.models;
 
 public class Drink {
+    // the size of the drink chosen by the customer. small medium or large
+    private String size;
+    // the flavor of the drink chosen by the customer
+    private String flavor;
+    // the price of the drink based on size. small $2.00 medium $2.50 large $3.00
+    private double price;
+
 }
