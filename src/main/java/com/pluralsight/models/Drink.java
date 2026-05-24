39af1291c977dae590ce32dcc4914f80be93fc05
michalhagos <mhayele3@gmail.com>
2026-05-24 00:21:34 -0500

    Add constructor to Drink that sets price from size

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -8,4 +8,17 @@ public class Drink {
     // the price of the drink based on size. small $2.00 medium $2.50 large $3.00
     private double price;
 
+    // constructor sets size and flavor. price is  set from the size chosen
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+        // update the price based on the size chosen by the customer
+        if (size.equalsIgnoreCase("small")) {
+            this.price = 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            this.price = 2.50;
+        } else if (size.equalsIgnoreCase("large")) {
+            this.price = 3.00;
+        }
+    }
 }
