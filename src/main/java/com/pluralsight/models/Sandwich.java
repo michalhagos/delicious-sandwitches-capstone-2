14f0fd9d8768c20196ec518be0c55b548e7fd6d7
michalhagos <mhayele3@gmail.com>
2026-05-23 21:37:37 -0500

    Add private fields bread size toasted toppings as array list and basePrice to Sandwich class

--- src/main/java/com/pluralsight/models/Sandwich.java
+++ src/main/java/com/pluralsight/models/Sandwich.java
@@ -1,4 +1,23 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+
 public class Sandwich {
-}
+        // the type of bread chosen by the customer
+        private String bread;
+        // the size of the sandwich in inches
+        private int size;
+        // whether the customer wants the sandwich toasted
+        private boolean toasted;
+        // the list of all toppings on this sandwich
+        private ArrayList<Topping> toppings;
+
+        // the starting price of the sandwich based on size only.topping prices get added on top of this
+        private double basePrice;
+
+
+
+
+
+    }
+
