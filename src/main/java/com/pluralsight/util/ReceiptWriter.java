52debbbf8892497b68662dd3a3bf68b4a91bf5fa
michalhagos <mhayele3@gmail.com>
2026-05-25 14:56:03 -0500

    Add generateTimestamp method to ReceiptWriter using DateTimeFormatter

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -1,4 +1,20 @@
 package com.pluralsight.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptWriter {
+    // generateTimestamp generates the current date and time as a string
+// it's used to create a unique filename for each receipt
+// format is yyyyMMdd-HHmmss. example 20230329-121523
+    private static String generateTimestamp() {
+        // get the current date and time
+        LocalDateTime now = LocalDateTime.now();
+        // format it to match the required filename format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        // return the formatted timestamp as a string
+        return now.format(formatter);
+    }
+
+
 }
