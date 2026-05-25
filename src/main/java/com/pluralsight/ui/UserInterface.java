d7e4d3890b41e1765514a705d5bd2df1818b3325
michalhagos <mhayele3@gmail.com>
2026-05-25 15:21:23 -0500

    imports all classes needed in UserInterface class

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,4 +1,12 @@
 package com.pluralsight.ui;
+import com.pluralsight.models.Chips;
+import com.pluralsight.models.Drink;
+import com.pluralsight.models.Order;
+import com.pluralsight.models.Sandwich;
+import com.pluralsight.models.Topping;
+import com.pluralsight.util.ReceiptWriter;
+import java.util.Scanner;
 
 public class UserInterface {
+
 }
